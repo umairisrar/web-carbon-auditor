9247142779136ca711a71b8dc699568ac51b9f46
umairisrar <umair_israr92@hotmail.com>
2022-08-02 14:41:52 +0500

    comment tunnel code

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-const { createTunnel } = require('./helpers/tunnel');
+//const { createTunnel } = require('./helpers/tunnel');
 
 const { PORT: port } = process.env;
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(routes);
 app.listen(port, () => {
-  createTunnel(port);
+ // createTunnel(port);
 });
 
 module.exports = app;
