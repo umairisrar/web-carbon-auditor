d84fa5c0a80e5d2cf670913bf049025e7f0ea082
umairisrar <umair_israr92@hotmail.com>
2022-08-27 05:43:11 +0500

    latest

--- src/controllers/monday-controller.js
+++ src/controllers/monday-controller.js
@@ -44,7 +44,7 @@ async function executeAction(req, res) {
     const { boardId, itemId, sourceColumnId, targetColumnId, transformationType } = inputFields;
 
     const allRowAttributes = await mondayService.getRowAtributes(shortLivedToken, itemId);
-    
+
     if (!allRowAttributes || allRowAttributes.length<1) {
       return res.status(200).send({});
     }
@@ -64,17 +64,26 @@ async function executeAction(req, res) {
     console.log(websiteColumn);
 
     var co2Column = allRowAttributes.find(item => item.title.toLowerCase() === "co2");
-    let co2ColumnId=-1,speedColumnId=-1,performanceColumnId=-1;
+    let co2ColumnId=-1,speedColumnId=-1,performanceColumnId=-1,co2SWDColumnId=-1,
+    unusedCSSBytesColumnId=-1,
+    unusedCSSSecondsColumnId=-1,unusedJavascriptBytesColumnId=-1,unusedJavascriptSecondsColumnId=-1,
+    energyPerVisitColumnId=-1,emissionsPerVisitInGramsColumnId=-1,annualEnergyInKwhColumnId=-1,annualEmissionsInGramsColumnId=-1;
+
     if(!co2Column){
       co2Column= await mondayService.createColumn(boardId,"CO2","text");
       
     }
-
     co2ColumnId = co2Column.id;
 
+    if(!co2SWDColumn){
+      co2SWDColumn= await mondayService.createColumn(boardId,"CO2 SWD","text");
+      
+    }
+    co2SWDColumnId = co2SWDColumn.id;
 
-    console.log('co2Column');
-    console.log(co2Column);
+
+    console.log('co2SWDColumn');
+    console.log(co2SWDColumn);
     
     var speedColumn = allRowAttributes.find(item => item.title.toLowerCase() === "speed");
 
@@ -100,7 +109,84 @@ async function executeAction(req, res) {
     console.log('performanceColumn');
     console.log(performanceColumn);
 
-    let auditColumnIds= {co2ColumnId,speedColumnId,performanceColumnId};
+
+    var unusedJavascriptBytesColumn = allRowAttributes.find(item => item.title.toLowerCase() === "unused javacript");
+    if(!unusedJavascriptBytesColumn){
+      unusedJavascriptBytesColumn = await mondayService.createColumn(boardId,"Unused Javacript","text");
+      
+    }
+    unusedJavascriptBytesColumnId = unusedJavascriptBytesColumn.id
+
+    
+    var unusedJavascriptSecondsColumn = allRowAttributes.find(item => item.title.toLowerCase() === "unused javacript seconds");
+    if(!unusedJavascriptSecondsColumn){
+      unusedJavascriptSecondsColumn = await mondayService.createColumn(boardId,"Unused Javacript Seconds","text");
+      
+    }
+    unusedJavascriptSecondsColumnId = unusedJavascriptSecondsColumn.id
+
+
+
+    var unusedCSSBytesColumn = allRowAttributes.find(item => item.title.toLowerCase() === "unused javacript");
+    if(!unusedCSSBytesColumn){
+      unusedCSSBytesColumn = await mondayService.createColumn(boardId,"Unused Javacript","text");
+      
+    }
+    unusedCSSBytesColumnId = unusedCSSBytesColumn.id
+
+
+    var unusedCSSSecondsColumn = allRowAttributes.find(item => item.title.toLowerCase() === "unused css seconds");
+    if(!unusedCSSSecondsColumn){
+      unusedCSSSecondsColumn = await mondayService.createColumn(boardId,"Unused CSS Seconds","text");
+      
+    }
+    unusedCSSSecondsColumnId = unusedCSSSecondsColumn.id
+    
+
+    var energyColumn = allRowAttributes.find(item => item.title.toLowerCase() === "energy per visit");
+    if(!energyColumn){
+      energyColumn = await mondayService.createColumn(boardId,"Energy Per Visit","text");
+      
+    }
+    energyPerVisitColumnId = energyColumn.id
+
+
+
+    var emissionPerVistGMColumn = allRowAttributes.find(item => item.title.toLowerCase() === "emission per visit(gms)");
+    if(!emissionPerVistGMColumn){
+      emissionPerVistGMColumn = await mondayService.createColumn(boardId,"Energy Per Visit","text");
+      
+    }
+
+    emissionsPerVisitInGramsColumnId = emissionPerVistGMColumn.id;
+
+
+    var annualEnergyColumn = allRowAttributes.find(item => item.title.toLowerCase() === "annual energy");
+    if(!annualEnergyColumn){
+      annualEnergyColumn = await mondayService.createColumn(boardId,"Annual Energy","text");
+      
+    }
+
+    annualEnergyInKwhColumnId = annualEnergyColumn.id;
+
+
+    var annualEmissionGMColumn = allRowAttributes.find(item => item.title.toLowerCase() === "annual emission");
+    if(!annualEmissionGMColumn){
+      annualEmissionGMColumn = await mondayService.createColumn(boardId,"Annual Emission","text");
+      
+    }
+
+    annualEmissionsInGramsColumnId = annualEmissionGMColumn.id;
+
+
+
+
+    let auditColumnIds= {
+      co2ColumnId,speedColumnId,performanceColumnId,
+      unusedJavascriptBytesColumnId,unusedJavascriptSecondsColumnId,unusedCSSBytesColumnId,
+      unusedCSSSecondsColumnId, energyPerVisitColumnId,emissionsPerVisitInGramsColumnId,
+      annualEnergyInKwhColumnId,
+      co2SWDColumnId,annualEmissionsInGramsColumnId};
 
    
     console.log('Audit Column Ids');
