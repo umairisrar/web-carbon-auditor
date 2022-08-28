943c223e146a1d61aa558ed3b142bc30400686eb
umairisrar <umair_israr92@hotmail.com>
2022-08-28 16:49:51 +0500

    remove extra route

--- src/routes/monday.js
+++ src/routes/monday.js
@@ -3,6 +3,5 @@ const { authenticationMiddleware } = require('../middlewares/authentication');
 const mondayController = require('../controllers/monday-controller');
 
 router.post('/monday/execute_action', authenticationMiddleware, mondayController.executeAction);
-router.post('/monday/get_remote_list_options', authenticationMiddleware, mondayController.getRemoteListOptions);
 
 module.exports = router;
