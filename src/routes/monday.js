d12b3e7825e454876bd7483e5ef43a247aa3d78e
umairisrar <umair_israr92@hotmail.com>
2022-09-02 18:54:46 +0500

    Update monday.js

--- src/routes/monday.js
+++ src/routes/monday.js
@@ -4,5 +4,6 @@ const mondayController = require('../controllers/monday-controller');
 
 router.post('/monday/execute_action', authenticationMiddleware, mondayController.executeAction);
 router.post('/monday/get_remote_list_options', authenticationMiddleware, mondayController.getRemoteListOptions);
+router.post('/monday/execute_email_action', authenticationMiddleware, mondayController.executeEmailAction);
 
 module.exports = router;
