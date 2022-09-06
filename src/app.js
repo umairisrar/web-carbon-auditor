11cecd066ab23f342b881e4dfa7be7e6487c5002
umairisrar <umair_israr92@hotmail.com>
2022-09-06 21:48:37 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,14 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-
+const { wakeDyno } = require('heroku-keep-awake');
 
 const { PORT: port } = process.env;
 const app = express();
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
 app.use(routes);
+
+
+const DYNO_URL = 'https://webauditor.herokuapp.com';
 app.listen(port, () => {
+  
+  const opts = {
+    interval: 29,
+    logging: false,
+    stopTimes: { start: '00:00', end: '06:00' }
+}
+
+wakeDyno(DYNO_URL, opts);
+  
 console.log('Server started...')
 });
 
